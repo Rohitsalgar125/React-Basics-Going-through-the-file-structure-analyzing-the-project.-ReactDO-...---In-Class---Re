00ebc0741cce2d346e1b467f91ddbccd39b26a4a
Rohitsalgar125 <101690901+Rohitsalgar125@users.noreply.github.com>
2022-06-14 08:57:36 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div>
+      <h1 className="data-ns-test">TIC TAC TOE</h1>
+      <p className="data-ns-test">
+        This Project made with the help of youtube using Tech Stack are HTML CSS
+        Javascript .
+      </p>
+    </div>
   );
 }
 export default App;
