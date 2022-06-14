488bd7135d0e1363ce48fc16d314d51cbc185d78
Rohitsalgar125 <101690901+Rohitsalgar125@users.noreply.github.com>
2022-06-14 08:58:52 +0530

    done1

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 function App() {
   return (
     <div>
-      <h1 className="data-ns-test">TIC TAC TOE</h1>
-      <p className="data-ns-test">
+      <h1 className="data-ns-test" value="project-name">
+        TIC TAC TOE
+      </h1>
+      <p className="data-ns-test" value="project-description">
         This Project made with the help of youtube using Tech Stack are HTML CSS
         Javascript .
       </p>
